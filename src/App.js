e115a34c09f6045f35f39c4461e6b90b0bc58923
Muhamad Baha Udin <mbaha713m@gmail.com>
2023-08-06 09:16:41 +0700

    update

--- src/App.js
+++ src/App.js
@@ -11,24 +11,22 @@ const App = () => {
     });
   }, []);
 
-  console.log({ popularMovies: popularMovies });
-
-  // const PopularMovieList = () => {
-  //   return popularMovies.map((movie, i) => {
-  //     return (
-  //       <div className="movie-wrapper" key={i}>
-  //         <div className="movie-title">{movie.title}</div>
-  //         <img
-  //           alt="banner"
-  //           className="movie-image"
-  //           src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
-  //         />
-  //         <div className="movie-date">{movie.release_date}</div>
-  //         <div className="movie-rate">{movie.vote_average}</div>
-  //       </div>
-  //     );
-  //   });
-  // };
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            alt="banner"
+            className="movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="movie-date">{movie.release_date}</div>
+          <div className="movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
 
   const Search = (q) => {
     console.log({ q });
@@ -43,7 +41,10 @@ const App = () => {
           className="movie-search"
           onChange={({ target }) => Search(target.value)}
         />
-        <div className="movie-container">{/* <PopularMovieList /> */}</div>
+        <div className="movie-container">
+          <div></div>
+          <PopularMovieList />
+        </div>
       </header>
     </div>
   );
