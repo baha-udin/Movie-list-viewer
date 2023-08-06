bbe224f1f5845e4f8f783303888081395cebb118
Muhamad Baha Udin <mbaha713m@gmail.com>
2023-08-06 08:48:18 +0700

    push project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getMovieList } from "./api";
+
+const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
+  }, []);
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            className="movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="movie-date">{movie.release_date}</div>
+          <div className="movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
+  };
+
+  const Search = (q) => {
+    console.log({ q });
+  };
+
+  console.log({ popularMovies: popularMovies });
+
+  return (
+    <div className="App">
+      <header className="App -header">
+        <h1>Udin movie mania</h1>
+        <input
+          placeholder="Search your favourite films"
+          className="movie-search"
+          onChange={({ target }) => Search(target.value)}
+        />
+        <div className="movie-container">
+          <PopularMovieList />
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default App;
